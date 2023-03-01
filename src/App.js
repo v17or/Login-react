cff69510812259706e8a8e63e84d32a3ec642041
v17or <vitor.sartori50@gmail.com>
2023-02-28 21:10:47 -0300

    Done!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-
+import { useState } from 'react';
+import './styles.css'
 function App() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
     <div className="container">
       <div className="container-login">
         <div className="wrap-login">
           <form className="login-form">
             <span className="login-form-title">Bem Vindo!</span>
-            
+
+            <div className="wrap-input">
+              <input 
+              className={email !== "" ? 'has-val input' : 'input'} 
+              type="e-mail" 
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="E-mail"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input 
+              className={password !== "" ? 'has-val input' : 'input'} 
+              type="password" 
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Login</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1">Nao possui conta?</span>
+              <a className="txt2" href="#">Criar conta.</a>
+            </div>
+
           </form>
         </div>
       </div>
